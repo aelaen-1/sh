437a3bcf525883d5cf95d480c8afc97a62b02473
aelaen-42 <aboukezi@bess-f4r4s8.clusters.42paris.fr>
2024-12-04 14:29:27 +0100

    rr

--- parsing.c
+++ parsing.c
@@ -1,28 +1,31 @@
+
 #include "include/minishell.h"
+// int is_space(char c)
+// {
+//     if (c >= 9 && c <= 13)
+//         return (1);
+//     return (0);
+// }
 
-int is_space(char c)
+// int is_builtin(char *s)
+// {
+//     if (s == "cd" || s == "echo" || s == "pwd" || s == "export" || s == "unset" || s == "env" || s == "exit")
+//         return (1);
+//     return (0);
+// }
+
+
+void    parse(char *inpt, char *delim)
 {
-    if (c >= 9 && c <= 13)
-        return (1);
-    return (0);
+    char    *buffer = malloc(strlen(inpt));
+    char    *input = strdup(inpt);
+    printf("\nOriginal: \"%s\"\n", input);
+    printf("\nTo split by: \"%s\"\n", delim);
+
+    char *a = NULL;
 }
 
-int is_builtin(char *s)
-{
-    if (s == "cd" || s == "echo" || s == "pwd" || s == "export" || s == "unset" || s == "env" || s == "exit")
-        return (1);
-    return (0);
-}
-t_command    *parse_input(char *input)
-{
-    t_command *cmd;
-    int     i;
 
-    i = 0;
-    get_first_token();
-
-    return (cmd);
-}
 
 int main(int ac, char **av)
 {
@@ -30,8 +33,15 @@ int main(int ac, char **av)
     (void)av;
 
     char *input = readline("minishell % ");
-    t_command *cmd = parse_input(input);
-    exec(cmd);
+
+    while (input)
+    {
+        printf("%s\n", strtok(input, " "));
+        strtok = strtok(NULL_ )
+    }
+    // parse(input, ' ');
+    // t_command *cmd = parse_input(input);
+    // exec(cmd);
     
     return (0);
 }
